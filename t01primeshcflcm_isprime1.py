bdfc4471479b3e72f3b29e06f2daaac7d72caeea
buliendA <52987932+buliendA@users.noreply.github.com>
2019-10-24 14:56:16 +0800

    finish is it a prime no.

--- t01primeshcflcm_isprime1.py
+++ t01primeshcflcm_isprime1.py
@@ -1,7 +1,7 @@
 # Is num a prime number? 
 # A prime number is an integer with exactly 2 different factors, 1 and itself.
 
-num = 61
+num = 66
 num_factors = 0
 for i in range(1,num+1):
   if num % i == 0:
